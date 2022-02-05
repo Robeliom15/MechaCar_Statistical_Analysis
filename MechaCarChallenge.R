e2afb004dcf66eb99354cab297dce8207f092890
Robeliom15 <robeliom15@gmail.com>
2022-02-05 09:32:28 -0800

    Dev 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,7 @@
+library(dplyr)
+
+setwd("C:/Users/super/Documents/Personal-DESKTOP-347GCA7/Bootcamp/Git Hub/MechaCar_Statistical_Analysis")
+
 #Dev 1
 
 # Import and read 
@@ -21,3 +25,16 @@ lot_sum <- suscoil %>% group_by(Manufacturing_Lot) %>% summarize(PSI_mean=mean(P
                                  PSI_var=var(PSI),
                                  PSI_SD=sd(PSI),
                                  .groups = 'keep') 
+
+#Dev3
+#1
+t.test(suscoil$PSI,mu=1500)
+
+#2
+lot1 <- subset(suscoil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(suscoil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(suscoil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
