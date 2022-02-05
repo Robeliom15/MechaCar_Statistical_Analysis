9b8055233edfb162a3f1e9d8d04811e64e49eb68
Robeliom15 <robeliom15@gmail.com>
2022-02-04 20:32:34 -0800

    Dev 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,18 @@ mechacar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar))
 
+#Dev 2
+#2
+suscoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+#3
+total_sum <- suscoil %>% summarize(PSI_mean=mean(PSI),
+                                   PSI_median=median(PSI),
+                                   PSI_var=var(PSI),
+                                   PSI_SD=sd(PSI),
+                                   .groups = 'keep') 
+#4
+lot_sum <- suscoil %>% group_by(Manufacturing_Lot) %>% summarize(PSI_mean=mean(PSI),
+                                 PSI_median=median(PSI),
+                                 PSI_var=var(PSI),
+                                 PSI_SD=sd(PSI),
+                                 .groups = 'keep') 
--- README.md
+++ README.md
@@ -6,8 +6,22 @@
 
 ## Linear Regression to Predict MPG
 
+![Dev 1 - Linear Regression to Predict](https://github.com/Robeliom15/MechaCar_Statistical_Analysis/blob/main/Images/Dev%201%20-%20Linear%20Regression%20to%20Predict%20MPG.png?raw=true)
+
+-
+
+-
+
+-
+
 ## Summary Statistics on Suspension Coils
+
+![alt text](http://url/to/img.png)
 
 ## T-Tests on Suspension Coils
 
+![alt text](http://url/to/img.png)
+
 ## Study Design: MechaCar vs Competition
+
+![alt text](http://url/to/img.png)
